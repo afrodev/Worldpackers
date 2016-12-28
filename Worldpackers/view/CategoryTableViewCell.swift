917e5e625cea288a8f82435dc264facbae6c5cf3
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-12-28 10:32:54 -0200

    arrumado autolayout da tableview category

---
+++ Worldpackers/view/CategoryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryTableViewCell.swift
+//  Worldpackers
+//
+//  Created by Humberto Vieira on 28/12/16.
+//  Copyright © 2016 Humberto Vieira. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageViewPhoto: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Worldpackers/viewcontroller/CategoryTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CategoryTableViewController.swift
+//  Worldpackers
+//
+//  Created by Humberto Vieira on 28/12/16.
+//  Copyright © 2016 Humberto Vieira. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    let arrayImages = [
+        UIImage(named: "hostel-life"),
+        UIImage(named: "social-impact"),
+        UIImage(named: "nature-lovers"),
+        UIImage(named: "social-media"),
+        UIImage(named: "photography"),
+        UIImage(named: "chef-to-the-world"),
+        UIImage(named: "hands-on"),
+        UIImage(named: "short-trip"),
+        UIImage(named: "long-jorney")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayImages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        (cell as! CategoryTableViewCell).imageViewPhoto.image = arrayImages[indexPath.row]
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellCategory", for: indexPath)
+
+        return cell
+    }
+
+}
